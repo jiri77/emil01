3fd784ff9c8a1b6643e3a3837ed200348245fa86
jiri77 <jiri.hlavacek@gmail.com>
2019-12-11 16:02:08 +0100

    Revert "k2"
    This reverts commit b528019a8d0013ffec6dbb1720e0044f70c602aa.
    
    # Conflicts:
    #	README.md

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ testuju
 napsal
 druhy napsal
 znovupřišel 10.12.19
+a ještě jednu větu 10.12.19
+a další notepadem++
--- zdroj1.js
+++ zdroj1.js
@@ -1,2 +1,7 @@
 #Co sem dám
 # vlastně
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
