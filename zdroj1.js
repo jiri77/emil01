3fbff118c4110e0613d068482668e9322264bf20
jihlcz <jiri.hlavacek@gmail.com>
2020-03-21 19:35:29 +0100

    pokus o push develop

--- zdroj1.js
+++ zdroj1.js
@@ -6,11 +6,8 @@
       .subscribe(hero => this.hero = hero);
 # vlastně
   }
-<<<<<<< HEAD
-i long = 798798;
-=======
+  i long = 798798;
   Array x = 5;
   long i = 79879;
   String pepa = "Pepa";
   
->>>>>>> druhý z vs do master
